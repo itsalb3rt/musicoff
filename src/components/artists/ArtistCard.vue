e95455cdd32080305148db6a62b652f2a53dec82
Albert Hidalgo <itsalb3rt@gmail.com>
2025-02-21 13:26:10 -0400

    Add ArtistCard component to display artist information

---
+++ src/components/artists/ArtistCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-card @click="$emit('details')" flat>
+    <q-card-section class="text-center">
+      <div>
+        <q-avatar size="50px" color="white" text-color="black">
+          {{ getTheFirstLetter(artist) }}
+        </q-avatar>
+      </div>
+      <div class="q-mt-sm ellipsis-2-lines">
+        {{ artist }}
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+defineProps({
+  artist: {
+    type: String,
+    required: true,
+  },
+})
+
+const getTheFirstLetter = (artist) => {
+  return artist.charAt(0).toUpperCase()
+}
+</script>
